fd01b75d20bc4c82f05adc12609f77e900ed047f
santi <santiagoc333@gmail.com>
2023-12-28 15:53:38 -0700

    altered schema to be more comprehensive

--- setup.sql
+++ setup.sql
@@ -5,12 +5,26 @@ SET search_path = public, face_recognition;
 -- Username: postgres
 -- TODO: make sure schema is good
 
+-- Schema Design:
+-- 2 main tables, photo_info stores all photos
+-- person stores each individual person in the database
+
 -- Ideally, everyone's face will be in this table, need to make sure that these are the appropriate attributes
-CREATE TABLE face_recognition.person_info (
-    person_id SERIAL PRIMARY KEY,
-    person_name VARCHAR(100) NOT NULL,
+CREATE TABLE face_recognition.photo_info (
+    photo_id SERIAL PRIMARY KEY, -- unique to each photo
+    person_id -- references people.person_id, assigned by user specification
     image_data BYTEA NOT NULL, -- Assuming storing images as byte data
     other_attributes JSONB, -- You can store additional attributes in JSON format
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Add another table for each unique person
+CREATE TABLE face_recognition.person (
+    person_id SERIAL PRIMARY KEY,
+    person_name VARCHAR(100) NOT NULL,
+    primary_photo BYTEA NOT NULL,
+    has_permission BOOLEAN,
+    notes JSONB,
+    user, -- allow user to set username and password ??? potentially
+    pass, 
+)
